6ae4479da4e1c2745725eed16e044311e7bd2a5d
nati2310 <160882161+nati2310@users.noreply.github.com>
2024-09-26 09:24:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+ // drama romanse comedia aventura açao 
+//minios 18 comedia aventura 
+//malevola 10 romance aventura açao 
+//crepuscolo 13 drama romanse comedia aventura 
+//os incriveis 6 drama comedia aventura 
+// a era do gelo; livre, açao comedia aventura 
+//divertidamente, livre, aventura e comedia
+//fantastica frabica de chocolate, livre, aventura e comedia
+//as aventiras de pi 10 aventura drama 
+//avatar, 14 aventura
+//mulher maravivia,12 açao drama aventura 
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
