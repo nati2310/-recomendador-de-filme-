e831d1da663ad67e2c89d581b9fade32b2724439
nati2310 <160882161+nati2310@users.noreply.github.com>
2024-10-17 08:18:05 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,10 +12,58 @@
 
 
 
+
+}
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("10");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
 }
 
 function draw() {
-  background(220);
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "malevola";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "mulher maravinha";          
+        } else{
+         return "as aventuras de pi";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "avatar";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "a fantastica fabrica de chocolat";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
 }
