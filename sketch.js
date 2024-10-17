833c231b86035bb225a39e17459be01205b85692
nati2310 <160882161+nati2310@users.noreply.github.com>
2024-10-17 09:10:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,23 +3,19 @@
 //malevola 10 romance aventura açao 
 //crepuscolo 13 drama romanse comedia aventura 
 //os incriveis 6 drama comedia aventura 
-// a era do gelo; livre, açao comedia aventura 
+// a era do gelo; livre, açao comedia aventura                         //  agora e recomendadir de livros esses nomes n valen mais 
 //divertidamente, livre, aventura e comedia
 //fantastica frabica de chocolate, livre, aventura e comedia
 //as aventiras de pi 10 aventura drama 
 //avatar, 14 aventura
 //mulher maravivia,12 açao drama aventura 
 
-
-
-
-}
 let campoIdade;
 let campoFantasia;
 let campoAventura;
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(700, 400);
   createElement("h2", "Recomendador de filmes");
   createSpan("Sua idade:");
   campoIdade = createInput("10");
@@ -34,7 +30,7 @@ function draw() {
   let gostaDeAventura = campoAventura.checked();
   let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
 
-  fill(color(76, 0, 115));
+  fill(color(76, 150, 115));
   textAlign(CENTER, CENTER);
   textSize(38);
   text(recomendacao, width / 2, height / 2);
@@ -43,27 +39,33 @@ function draw() {
 function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if (idade >= 10) {
     if (idade >= 14) {
-      return "malevola";
+      return "harri potter";
     } else {
-      if (idade >= 12) {
+      if (idade >= 12){
         if(gostaDeFantasia || gostaDeAventura) {
-          return "mulher maravinha";          
+          return "as cronicas de narnia";          
         } else{
-         return "as aventuras de pi";
+         return "como eu era antes de voce";
         }
       } else {
         if (gostaDeFantasia) {
-          return "As aventuras de pi";
+          return "como eu era antes de voce";
         } else {
-          return "avatar";
+          return "jogos vorazes";
         }
       }
     }
   } else {
     if (gostaDeFantasia) {
-      return "a fantastica fabrica de chocolat";
+      return "vermenho branco e sange azul";
     } else {
-      return "O feitiço do tempo";
+      return "heartstopper";
     }
   }
 }
+
+
+
+
+ 
+    
